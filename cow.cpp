becaddc5d4f34ad8266411ba9ed2248c23bc4c0a
rida mushtaq <mushtaqrida06@gmail.com>
2022-12-20 18:48:46 -0800

    PD

---
+++ cow.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main () {
+system ("color 30");
+cout << "--------------------------------------------- " << endl;       
+cout << " o      ^____^                                " << endl;
+cout << "    o   ( oo )\\______________                 " << endl;
+cout << "                                                 " << endl;                                                     
+cout << "        (____)\\              )\\/\\             " << endl;
+cout << "              | |---------W  |                " << endl;
+cout << "                                              " << endl;
+cout << "              | |          | |                " << endl;
+
+}
